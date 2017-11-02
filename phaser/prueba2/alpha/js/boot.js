c8c856e7089c3526d37306bee22cb3f267a90325
alexander <alexaj7_@hotmail.com>
2017-11-01 20:28:08 -0400

    first commit

--- phaser/prueba2/alpha/js/boot.js
+++ phaser/prueba2/alpha/js/boot.js
@@ -1,26 +1,28 @@
-//Objeto balies 
+// game = Object
 
 var Boot = Boot || {};
 
-//Crear clase Boot
+// creaate clase Boot
+
 Boot = {
 	preload: function () {
 		musicaBtn: '',
-		balies.load.image('fondoIni', 'sprites/fondoInicio.png');
-		balies.load.spritesheet('boton', 'sprites/botones.png', 180, 80);
-		balies.load.audio('musicaIni', 'audio/oedipus.mp3');
-		balies.load.audio('clicBtn', 'audio/ping.mp3');
-		},
-	create: function () {
-		balies.add.sprite(0, 0, 'fondoIni');
-		var musica = balies.add.audio('musicaIni');
-		musica.play('', 0, 0.5, true);
-		this.musicaBtn = balies.add.audio('clicBtn');
-		
-		balies.add.button(400, 300, 'boton', this.iniciar, this, 2, 1, 0);
-		},
-		iniciar: function () {
-			this.musicaBtn.play();
-			console.log('Inicia videojuego.');			
-			}
+		game.load.image('tile', 'sprites/fondoInicio.png');
+		game.load.spritesheet('boton', 'sprites/botones.png', 180 ,80);
+		game.load.audio('musicaIni', 'audio/oedipus.mp3');
+		game.load.audio('clickBtn', 'audio/ping.mp3');
+	},
+	create:function() {
+		game.add.sprite(0,0, 'tile');
+		var musica = game.add.audio('musicaIni');
+		musica.play('',0 , 0.3, true);
+		this.musicaBtn = game.add.audio('clickBtn');
+
+		game.add.button(400,300, 'boton',this.iniciar , this , 2,1,0);
+	},
+
+	iniciar: function() {
+		console.log('inicia videojuego');
+		this.musicaBtn.play('',0,0.2);
+	}
 }
